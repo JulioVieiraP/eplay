47dd6ca23d9983ddc026f573c9a36241b25bc265
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-05-29 11:45:10 -0300

    feat: criando o metodo post para api

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -6,37 +6,60 @@ import Carrinho from '../../assets/images/carrinho.svg'
 import { useDispatch, useSelector } from 'react-redux'
 import { open } from '../../redux/reducers/cart'
 import { RootReducer } from '../../redux/store'
+import { useState } from 'react'
 
 const Header = () => {
   const dispatch = useDispatch()
   const { items } = useSelector((state: RootReducer) => state.cart)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const OpenCart = () => {
     dispatch(open())
   }
   return (
     <S.HeaderBar>
-      <div>
-        <Link to="/">
-          <img src={Logo} alt="EPLAY" />
-        </Link>
-        <nav>
-          <S.Links>
-            <S.LinkItems>
-              <Link to="/Categorias">Categorias</Link>
-            </S.LinkItems>
-            <S.LinkItems>
-              <Link to="/">Novidades</Link>
-            </S.LinkItems>
-            <S.LinkItems>
-              <Link to="/">Promoções</Link>
-            </S.LinkItems>
-          </S.Links>
-        </nav>
-      </div>
-      <S.CartButton href="#" onClick={OpenCart}>
-        {items.length} - produto(s) <img src={Carrinho} alt="Carrinho" />
-      </S.CartButton>
+      <S.HeaderRow>
+        <div>
+          <S.Hamburguer onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <span />
+            <span />
+            <span />
+          </S.Hamburguer>
+          <Link to="/">
+            <img src={Logo} alt="EPLAY" />
+          </Link>
+          <nav>
+            <S.Links>
+              <S.LinkItems>
+                <Link to="/Categorias">Categorias</Link>
+              </S.LinkItems>
+              <S.LinkItems>
+                <Link to="/">Novidades</Link>
+              </S.LinkItems>
+              <S.LinkItems>
+                <Link to="/">Promoções</Link>
+              </S.LinkItems>
+            </S.Links>
+          </nav>
+        </div>
+        <S.CartButton href="#" onClick={OpenCart}>
+          {items.length}
+          <span> - produto(s)</span> <img src={Carrinho} alt="Carrinho" />
+        </S.CartButton>
+      </S.HeaderRow>
+      <S.NavMobile className={isMenuOpen ? 'is-open' : ''}>
+        <S.Links>
+          <S.LinkItems>
+            <Link to="/Categorias">Categorias</Link>
+          </S.LinkItems>
+          <S.LinkItems>
+            <Link to="/">Novidades</Link>
+          </S.LinkItems>
+          <S.LinkItems>
+            <Link to="/">Promoções</Link>
+          </S.LinkItems>
+        </S.Links>
+      </S.NavMobile>
     </S.HeaderBar>
   )
 }
--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -2,37 +2,74 @@ import styled from 'styled-components'
 import { cores } from '../../styles/Variaveis'
 import { breakpoints } from '../../styles/Globalstyle'
 
+export const Links = styled.ul`
+  display: flex;
+  margin-left: 40px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-left: 0;
+    display: block;
+  }
+`
+
 export const HeaderBar = styled.header`
   background-color: ${cores.cinza};
   padding: 24px;
   border-radius: 16px;
   margin-bottom: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  div {
-    display: flex;
-    align-items: center;
-  }
 
   a {
     color: ${cores.branca};
     font-weight: bold;
   }
+`
 
-  @media (max-width: ${breakpoints.tablet}) {
-    display: none;
+export const HeaderRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  > div {
+    display: flex;
+    align-items: center;
+
+    @media (max-width: ${breakpoints.tablet}) {
+      flex: 1;
+      justify-content: space-between;
+      ${Links} {
+        display: none;
+      }
+    }
   }
 `
 
-export const Links = styled.ul`
-  display: flex;
-  margin-left: 40px;
+export const NavMobile = styled.nav`
+  display: none;
+
+  @media (max-width: ${breakpoints.desktop}) {
+    &.is-open {
+      display: none;
+    }
+  }
+  @media (max-width: ${breakpoints.tablet}) {
+    &.is-open {
+      display: block;
+    }
+  }
 `
 
 export const LinkItems = styled.li`
   margin-right: 16px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-right: 0;
+
+    a {
+      display: block;
+      padding: 16px 0;
+      text-align: center;
+    }
+  }
 `
 
 export const CartButton = styled.a`
@@ -40,5 +77,26 @@ export const CartButton = styled.a`
 
   img {
     margin-left: 16px;
+  }
+
+  @media (max-width: ${breakpoints.tablet}) {
+    span {
+      display: none;
+    }
+  }
+`
+
+export const Hamburguer = styled.div`
+  width: 32px;
+  span {
+    display: block;
+    height: 2px;
+    width: 100%;
+    background-color: ${cores.branca};
+    margin-bottom: 4px;
+  }
+
+  @media (min-width: ${breakpoints.tablet}) {
+    display: none;
   }
 `
